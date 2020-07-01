8bfd2074973812a94bf22d8abcb4a11e5a50e0e3
dfegnoux <denis.fegnoux@gmail.com>
2020-07-01 17:18:01 +0200

    arrange a bit files

---
+++ aliases/git.sh
@@ -0,0 +1,28 @@
+alias gpl="git pull"
+alias gph="git push"
+alias gr="git rebase"
+alias gca="git commit --ammend"
+alias gcan="git commit --amend --no-edit"
+alias gap="git add -p"
+
+# Update a branch by update master then rebase the current branch with local master
+upbranch () {
+	git checkout master
+	git pull --rebase
+	git checkout -
+	git rebase master
+}
+
+# Update master then create a new branch from it with an ID passed at first argument
+startTask () {
+	git checkout master
+	git pull --rebase
+	git checkout -b LBCFTPA-$1
+}
+
+# Fetch master and remove all merged branches
+cleanBranch () {
+	git checkout master
+	git pull --rebase
+  git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d
+}
